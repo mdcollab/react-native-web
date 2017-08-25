6153b3d722be256ecd71fae47bb0ce13dcd9c972
Tom Berry <tberry860@gmail.com>
2017-08-25 10:55:01 -0700

    add refresh control stub

---
+++ src/components/RefreshControl/index.js
@@ -0,0 +1,16 @@
+/**
+ * @flow
+ */
+
+import View from '../View';
+import React, { Component } from 'react';
+
+class RefreshControl extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+module.exports = RefreshControl;
